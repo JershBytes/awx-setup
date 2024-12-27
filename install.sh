c2eb79b7ce6bc54509380948a2fc64908b8b654e
JershBytes <git@rossjm.dev>
2024-12-27 14:24:35 -0500

    finished draft of script.

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 
 # variables
-awx_clone_path="/opt/awx"
-awx_tag='2.19.1'
+AWX_GIT_URL=git@github.com:ansible/awx-operator.git
+AWX_CLONE_PATH="/opt/awx-operator"
+AWX_TAG='2.19.1'
+KUSTOMIZATION_URL="https://raw.githubusercontent.com/JershBytes/awx-setup/refs/heads/main/config/kustomization.yaml"
+AWX_YAML_URL="https://github.com/JershBytes/awx-setup/blob/main/config/awx.yml"
+FILENAME="kustomization.yaml"
+HOST_IP=$(hostname -I | cut -d' ' -f1)
 
 # dependencies function
 install-dependencies() {
@@ -11,7 +16,7 @@ install-dependencies() {
 curl -sfL https://get.k3s.io | sh -
 
 # Install dependencies
-yum -y install git make jq vim
+yum -y install git make jq vim curl wget ansible
 }
 
 ### AWX Setup ###
@@ -20,19 +25,43 @@ yum -y install git make jq vim
 install-dependencies
 
 # Clone awx repo
-git clone git@github.com:ansible/awx-operator.git "$awx_clone_path" && cd /opt/awx || exit
+git clone "$AWX_GIT_URL" "$AWX_CLONE_PATH" && cd "$AWX_CLONE_PATH" || exit
 
 # set tag version
-git checkout "$awx_tag"
+git checkout "$AWX_TAG"
 
 # Create awx
 make deploy
 
+# Grab kustomization.yaml
+curl -O $KUSTOMIZATION_URL
+
 # Install the manifests and check to see if its running after
-kubectl apply -k . && kubectl get pods -n awx
+kubectl apply -k . && watch -n5 kubectl get pods -n awx
 
 # set the current namespace for kubectl
 kubectl config set-context --current --namespace=awx
+
+# grab awx.yml file
+curl -O $AWX_YAML_URL && sed -i 's/^# - awx.yml/  - awx.yml/' $FILENAME
+
+# Confirm the change
+echo "Updated $FILENAME:"
+cat $FILENAME
+
+# Apply the changes
+kubectl apply -k .
+
+# Grab port info
+kubectl get svc -l "app.kubernetes.io/managed-by=awx-operator"
+
+# Job's done
+echo "AWX has been installed successfully and should now be running at http://${HOST_IP}, use the password below to sign in."
+
+# Grab admin password
+kubectl get secret awx-demo-admin-password -o jsonpath="{.data.password}" | base64 --decode ; echo
+
+
 
 
 
